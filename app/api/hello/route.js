b3ce0915939c9202fb14bcb1caadb42611dee637
TacticalWolves <arunvl88@gmail.com>
2024-05-23 11:34:31 -0700

    dsa

--- app/api/hello/route.js
+++ app/api/hello/route.js
@@ -2,56 +2,17 @@ import { getRequestContext } from '@cloudflare/next-on-pages'
 
 export const runtime = 'edge'
 
-/*
+
 export async function GET(request) {
-  let responseText = 'Hello World'
+  const responses = [
+    'Hello, how are you?',  // English
+    'Hola, ¿cómo estás?',   // Spanish
+    'Bonjour, comment ça va?', // French
+    'Hallo, wie geht es dir?'  // German
+  ];
 
-  // In the edge runtime you can use Bindings that are available in your application
-  // (for more details see:
-  //    - https://developers.cloudflare.com/pages/framework-guides/deploy-a-nextjs-site/#use-bindings-in-your-nextjs-application
-  //    - https://developers.cloudflare.com/pages/functions/bindings/
-  // )
-  //
-  // KV Example:
-  // const myKv = getRequestContext().env.MY_KV_NAMESPACE
-  // await myKv.put('suffix', ' from a KV store!')
-  // const suffix = await myKv.get('suffix')
-  // responseText += suffix
+  const randomIndex = Math.floor(Math.random() * responses.length);
+  const responseText = responses[randomIndex];
 
-  return new Response(responseText)
+  return new Response(responseText);
 }
-*/
-export default {
-	async fetch(request, env, ctx) {
-	  // Check if the request method is POST
-	  if (request.method === 'GET') {
-		// Define key value pairs
-		const randomMessage = {
-			1: "Hola 1",
-			2: "Hola 2",
-			3: "Hola 3",
-			4: "Hola 4",
-		  };
-
-		const max = 4;
-		
-		const random_number = Math.floor(Math.random() * max) + 1;
-
-		const OUTPUT = { Message: randomMessage[random_number]};
-
-		// Set the Content-Type header to application/json
-		const headers = {
-			'Content-Type': 'application/json'
-		};
-
-		// Return a JSON response with status code 200 OK
-		return new Response(JSON.stringify(OUTPUT), {
-		  status: 200,
-		  headers: headers
-		});
-	  } else {
-		// For other request methods, return the default response
-		return new Response('Use GET request');
-	  }
-	},
-  };
