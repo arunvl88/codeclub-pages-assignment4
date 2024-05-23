dd48c119b44ec66b5638345abd9d7def86c87bd7
TacticalWolves <arunvl88@gmail.com>
2024-05-23 10:59:50 -0700

    assignment4

--- app/api/hello/route.js
+++ app/api/hello/route.js
@@ -2,6 +2,7 @@ import { getRequestContext } from '@cloudflare/next-on-pages'
 
 export const runtime = 'edge'
 
+/*
 export async function GET(request) {
   let responseText = 'Hello World'
 
@@ -19,3 +20,38 @@ export async function GET(request) {
 
   return new Response(responseText)
 }
+*/
+export default {
+	async fetch(request, env, ctx) {
+	  // Check if the request method is POST
+	  if (request.method === 'GET') {
+		// Define key value pairs
+		const randomMessage = {
+			1: "Hola 1",
+			2: "Hola 2",
+			3: "Hola 3",
+			4: "Hola 4",
+		  };
+
+		const max = 4;
+		
+		const random_number = Math.floor(Math.random() * max) + 1;
+
+		const OUTPUT = { Message: randomMessage[random_number]};
+
+		// Set the Content-Type header to application/json
+		const headers = {
+			'Content-Type': 'application/json'
+		};
+
+		// Return a JSON response with status code 200 OK
+		return new Response(JSON.stringify(OUTPUT), {
+		  status: 200,
+		  headers: headers
+		});
+	  } else {
+		// For other request methods, return the default response
+		return new Response('Use GET request');
+	  }
+	},
+  };
